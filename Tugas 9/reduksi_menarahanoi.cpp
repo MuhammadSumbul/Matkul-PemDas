d746fd3e055b970a6f15f217bb3e3953774c8693
MuhammadSumbul <125243351+MuhammadSumbul@users.noreply.github.com>
2024-03-13 11:32:34 +0700

    Tugas 9 (Reduksi)

---
+++ Tugas 9/reduksi_menarahanoi.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <conio.h>
+
+ void hanoi(int piringan, char dari, char bantu, char ke){
+ 	if(piringan>0){
+	 hanoi(piringan-1, dari, ke, bantu);
+	 printf("Pindahkan piringan %d dari %c ke %c\n",piringan, dari, ke);
+	 hanoi(piringan-1, bantu, dari, ke);}
+	 } 
+	 
+main(){
+int piringan;
+printf("Berapa banyak piringan ? ");
+scanf("%d", &piringan);printf("\n");
+hanoi(piringan, 'A', 'B', 'C');
+getch();}
